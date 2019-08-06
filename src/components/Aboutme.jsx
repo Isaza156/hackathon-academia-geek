0bf3d274a955eef7816c8e8893c3a4729b048dc5
juan1445 <juanzuleta1445@gmail.com>
2019-08-06 16:17:24 -0500

    Listo el About sobre Makaia

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import About1 from '../img/quienessomos.jpg';
-import About2 from '../img/quehacemos.jpg';
-import About3 from '../img/queaprenderas.jpg';
 import './style/AboutmeStyle.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class AboutMe extends Component {
   state = {};
@@ -12,17 +10,17 @@ class AboutMe extends Component {
           <h1 className="mb-5" >Bienvenido a Academia Geek !!</h1>
         <div className="row">
           <div className="col-md-4 col-lg-4">
-            <a className="img-transition" href="/"><img src={About1} alt="" className="img-fluid shadow rounded-circle mb-4" /></a>
+            <FontAwesomeIcon icon="question" size="7x" className="p-2" color="orange"/>
             <h3>¿Quiénes somos?</h3>
             <p>Somos una alianza entre MAKAIA, Agile Innova y E-volution: tres organizaciones que trabajan con tecnología, innovación y soluciones digitales que han decidido compartir conocimiento a través de una Academia de Código y Programación.</p>
           </div>
-          <div className="col-md-4 col-lg-4">
-            <img src={About2} alt="" className="img-fluid shadow rounded-circle mb-4" />
+          <div className="col-md-4 col-lg-4 shadow">
+          <FontAwesomeIcon icon="tools" size="7x" className="p-2" color="orange"/>
             <h3>¿Qué hacemos?</h3>
             <p>Te ayudamos a que en 8 semanas logres adquirir y practicar competencias de desarrollo junior de aplicaciones web, preparándote así para comenzar tu camino en el emprendimiento tecnológico o trabajar con alguno de nuestros aliados.</p>
           </div>
           <div className="col-md-4 col-lg-4">
-            <img src={About3} alt="" className="img-fluid shadow rounded-circle mb-4" />
+          <FontAwesomeIcon icon="chalkboard-teacher" size="7x" className="p-2" color="orange"/>
             <h3>¿Qué aprendéras?</h3>
             <p>Además de programar, fortalecerás otras competencias y trabajarás con metodologías ágiles de desarrollo de software y emprendimiento TIC. Además te apoyaremos en la búsqueda de empleo y relacionamiento con empresas.</p>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChalkboardTeacher, faQuestion, faTools } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab, faChalkboardTeacher, faQuestion, faTools);
 
 ReactDOM.render(<App />, document.getElementById('app'));
